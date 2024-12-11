70ddf75d48606f24acb5c527c9a26c50bb19d5b4
Henok Samson <henoksamson57@gmail.com>
2024-12-11 13:09:10 +0300

    refactor RedisClient methods for consistent formatting and readability

--- utils/redis.js
+++ utils/redis.js
@@ -2,29 +2,29 @@ const redis = require('redis');
 const { promisify } = require('util');
 
 class RedisClient {
-  constructor() {
-    this.client = redis.createClient({ host: '127.0.0.1', port: 6379 });
-    this.getAsync = promisify(this.client.get).bind(this.client);
-    this.client.on('error', (error) => {
-      console.log(`Redis client not connected to the server: ${error.message}`);
-    });
-  }
+	constructor() {
+		this.client = redis.createClient({ host: '127.0.0.1', port: 6379 });
+		this.getAsync = promisify(this.client.get).bind(this.client);
+		this.client.on('error', (error) => {
+			console.log(`Redis client not connected to the server: ${error.message}`);
+		});
+	}
 
-  isAlive() {
-    return this.client.connected;
-  }
+	isAlive() {
+		return this.client.connected;
+	}
 
-  async get(key) {
-    return this.getAsync(key);
-  }
+	async get(key) {
+		return this.getAsync(key);
+	}
 
-  async set(key, value, duration) {
-    this.client.setex(key, duration, value);
-  }
+	async set(key, value, duration) {
+		this.client.setex(key, duration, value);
+	}
 
-  async del(key) {
-    this.client.del(key);
-  }
+	async del(key) {
+		this.client.del(key);
+	}
 }
 
 const redisClient = new RedisClient();
