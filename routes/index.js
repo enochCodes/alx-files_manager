593d8b5d989af320edbf0467d353f67ec3edc7ac
Henok Samson <henoksamson57@gmail.com>
2024-12-13 15:21:57 +0300

    refactor routing in index.js to use UserController and add authentication endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
+const UserController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
 export default (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
-  app.post('/users', UsersController.postNew);
+  app.post('/users', UserController.postNew);
+  app.get('/connect', AuthController.getConnect);
+  app.get('/disconnect', AuthController.getDisconnect);
+  app.get('/users/me', UserController.getMe);
 };
